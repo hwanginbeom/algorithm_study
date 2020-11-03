c9a41846e2561123d08db4a8db51c582cb7db2ba
wooooooooooz <37871545+wooooooooooz@users.noreply.github.com>
2020-11-03 23:28:46 +0900

    Update 20.11.01_woojin.py

--- 2.algorithm_test/20.11.01/20.11.01_woojin.py
+++ 2.algorithm_test/20.11.01/20.11.01_woojin.py
@@ -1,16 +1,13 @@
-## 수정중.....
-## git push 테스트중
+############################################ 1번문제 #########################################
 
-########### 1번 문제
-
-def solution(answers):
+def solution1(answers):
     answer = []
 
     person1 = [1, 2, 3, 4, 5]  # 5개
     person2 = [2, 1, 2, 3, 2, 4, 2, 5]  # 8개
     person3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]  # 10개
 
-    point_list = [0, 0, 0]
+    point_list = [0]*3
 
     for i in range(len(answers)):
         if answers[i] == person1[i % 5]:
@@ -20,59 +17,51 @@ def solution(answers):
         if answers[i] == person3[i % 10]:
             point_list[2] += 1
 
-    # [3,7,1]
-    maxindex = 0
-    print(point_list)
-    tie = []
+    score_leader = max(point_list)
 
-    for q in range(len(point_list)):
-        if point_list[q] > point_list[maxindex]:
-            maxindex = q
-            tie = []
-        elif point_list[q] == point_list[maxindex]:
-            tie.append(q)
-
-    if len(tie) == 0:
-        answer.append(maxindex + 1)
-    else:
-        for a in tie:
-            answer.append(a + 1)
+    for i in range(len(point_list)):
+        if score_leader  == point_list[i]:
+            answer.append(i + 1)
     return answer
 
 
-print(solution([3, 3, 1, 1, 2, 2, 4, 4, 5, 5,5,1,2,3,5,8 ]))
+answers = [1, 3, 2, 4, 2]
+print(solution1(answers))
 
 
-##### 2번문제
+############################################ 2번문제 #########################################
 
-# n = 2 ~ 30
 
-def solution(n, lost, reserve):
+def solution2(n, lost, reserve):
     answer = 0
 
+    # 여분있는 사람 중 도둑맞은 애들 제외시키기
     for r_person in reserve:
         for l_person in lost:
             if r_person == l_person:
                 reserve.remove(r_person)
-
-            ## reserve 에만 뺄 게 아니라 lost에도 뺴야된다 !!!!!
-            ## 우석 ) intersection
+                lost.remove(l_person)
 
     real_reserve = reserve.copy()
     clothes = n - len(lost)
+    answer = 0
 
-    for r in real_reserve:
-        for i in lost:
-            if r - 1 == i:
-                answer += 1
-                real_reserve.remove(r)
-                break
-            elif r + 1 == i:
-                answer += 1
-                real_reserve.remove(r)
+    # 빌려줄 애들 찾기 
+    for student in lost:
+        if (student - 1) in reserve:
+            reserve.remove(student - 1)
+            answer += 1
+            continue
+        elif (student + 1) in reserve:
+            reserve.remove(student + 1)
+            answer += 1
+            continue
 
     answer = answer + clothes
-
     return answer
 
-print(solution(3, [3], [1]))
+print(solution2(5, [2,4], [1,3,5]))
+
+
+
+##### 3번문제
