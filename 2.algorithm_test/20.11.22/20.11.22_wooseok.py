1bf6c74af3b9327691a427aecadb2ad6df485dff
WooSeok Choi <wsc9150@gmail.com>
2020-11-24 20:23:53 +0900

    Modify

--- 2.algorithm_test/20.11.22/20.11.22_wooseok.py
+++ 2.algorithm_test/20.11.22/20.11.22_wooseok.py
@@ -1,4 +1,47 @@
 # 1. N으로 표현
+def solution1(N, number) :
+    if N == number:
+        return 1
+
+    # 1. [ SET x 8 ] 초기화
+    s = [set() for x in range(8)]
+
+    # 2. 각 set마다 기본 수 "N" * i 수 초기화
+    for i, x in enumerate(s, start=1):
+        x.add(int(str(N) * i))
+
+    # 3. n 일반화
+    #   {
+    #       "n" * i U
+    #       1번 set 사칙연산 n-1번 set U
+    #       2번 set 사칙연산 n-2번 set U
+    #       ...
+    #       n-1번 set 사칙연산 1번 set,
+    #    }
+    # number를 가장 최소로 만드는 수 구함.
+    for i in range(1, 8):
+        for j in range(i):
+            for op1 in s[j]:
+                for op2 in s[i - j - 1]:
+                    s[i].add(op1 + op2)
+                    s[i].add(op1 - op2)
+                    s[i].add(op1 * op2)
+                    if op2 != 0:
+                        s[i].add(op1 // op2)
+
+        if number in s[i]:
+            answer = i + 1
+            break
+
+    else:
+        answer = -1
+
+    return answer
+
+
+N = 5
+number = 12
+print(solution1(N, number))
 
 
 
@@ -109,4 +152,4 @@ def solution3(n, edge):
 
 n = 6
 vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-print(solution3(n, vertex))
+# print(solution3(n, vertex))
