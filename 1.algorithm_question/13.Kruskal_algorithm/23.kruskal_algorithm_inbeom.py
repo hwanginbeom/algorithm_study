b0fd1b2531a10bc5a3ab711731dff10177506fbb
hwanginbeom <rydn2002@gmail.com>
2020-11-26 00:07:16 +0900

    20.11.26 | 13.kruskal_algorithm

---
+++ 1.algorithm_question/13.Kruskal_algorithm/23.kruskal_algorithm_inbeom.py
@@ -0,0 +1,89 @@
+# 9372 - 상근이의 여행
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 떄까지 재귀 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+test_cases = int(input())
+
+for _ in range(test_cases):
+    # 노드의 개수와 간선의 개수 입력받기
+    v, e = map(int, input().split())
+    parent = [0] * (v + 1)  # 부모 테이블 초기화 하기
+    edges = []
+    result = 0
+
+    # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+    for i in range(1, v + 1):
+        parent[i] = i
+    for _ in range(e):
+        a, b = map(int, input().split())
+        # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+        edges.append((1, a, b))
+    edges.sort()
+
+    for edge in edges:
+        cost, a, b = edge
+        # 사이클이 발생하지 않는 경우에만 집합에 포함
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            result += cost
+    print(result)
+
+
+# 1197 - Minimum spanning tree
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 떄까지 재귀 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1)  # 부모 테이블 초기화 하기
+edges = []
+result = 0
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+print(result)
