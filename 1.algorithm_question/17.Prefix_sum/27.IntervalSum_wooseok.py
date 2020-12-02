099b8f9469118359c2dd1a6ffadaaab7f71e26ab
WooSeok Choi <wsc9150@gmail.com>
2020-12-02 21:23:42 +0900

    20.12.02 | add algorithm

---
+++ 1.algorithm_question/17.Prefix_sum/27.IntervalSum_wooseok.py
@@ -0,0 +1,57 @@
+# 1. 구간 합 구하기 4
+def solution1() :
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    valueSum = 0
+    intervalSum = [0]
+
+    for a in array :
+        valueSum += a
+        intervalSum.append(valueSum)
+
+    for _ in range(m) :
+        i, j = map(int, input().split())
+        print(intervalSum[j] - intervalSum[i - 1])
+
+
+# solution1()
+
+
+
+# 2. 구간 합 구하기 5
+import sys
+
+
+def solution2() :
+    n, m = map(int, input().split())
+
+    array = []
+    for _ in range(n) :
+        # map(int, input().split())하면 시간 초과 난다....
+        temp = list(map(int, sys.stdin.readline().split()))
+        array.append(temp)
+
+    # 각 행의 접두사합 구하기
+    for i in range(n):
+        for j in range(n):
+            if j == 0:
+                pass
+            else:
+                array[i][j] += array[i][j - 1]
+
+    for _ in range(m) :
+        valueSum = 0
+        # 여기도...
+        x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+
+        for i in range(x1 - 1, x2) :
+            if y1 != 1 :
+                valueSum -= array[i][y1 - 2]
+
+            valueSum += array[i][y2 - 1]
+
+        print(valueSum)
+
+
+solution2()
