66856f17d845f063d5156f9dc824126bc5939c75
GyeongHyeon <lkh6035@naver.com>
2021-01-05 12:10:50 +0900

    21.01.05 | implemented_ACM호텔

--- 1.algorithm_question/2.implemented/45.implemented_gyeonghyeon.py
+++ 1.algorithm_question/2.implemented/45.implemented_gyeonghyeon.py
@@ -1,5 +1,4 @@
 n = int(input())
 for _ in range(n):
     H, W, N = map(int, input().split())
-    list_1 = [ str(i2+1)+str(i+1).zfill(2) for i in range(W) for i2 in range(H)]
-    print(list_1[N-1])
+    print([ str(i2+1)+str(i+1).zfill(2) for i in range(W) for i2 in range(H)][N-1])
