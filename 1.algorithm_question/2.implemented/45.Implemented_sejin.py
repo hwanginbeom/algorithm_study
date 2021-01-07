d3f3d577ab945ff095d048cfa282c7b65787ef57
Park SeJin <sjsj9368@gmail.com>
2021-01-08 00:29:15 +0900

    2021.01.08 | modify algorithm

--- 1.algorithm_question/2.implemented/45.Implemented_sejin.py
+++ 1.algorithm_question/2.implemented/45.Implemented_sejin.py
@@ -1,5 +1,18 @@
-N = int(input())
-
-for _ in range(N) :
+t = int(input())
+for _ in range(t) :
+    # 호텔 층 수, 각 층 방 수, 몇 번쨰 손님
     h, w, n = map(int, input().split())
-    print([ str(i2+1)+str(i+1).zfill(2) for i in range(w) for i2 in range(h)][n-1])
+
+    count = 0
+    x,y = 0,0
+
+    for i in range(w) :
+        for j in range(h) :
+            count += 1
+            if count == n :
+                x,y = i,j
+                break
+
+    # zfill(width) : 정해진 길이에서 앞에 0을 붙여주고 싶을 때
+    # rjust(width, [fillchar])
+    print(str(y+1) + str(x+1).zfill(2))
