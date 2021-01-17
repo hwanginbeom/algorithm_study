9a18ab1271a063060eb12d1291a01f3bb9e01440
Kyounglin-Kim <kim232311@gmail.com>
2021-01-17 22:41:43 +0900

    2021-01-17 | add algorithm_coding_test

---
+++ 2.algorithm_test/21.01.17/비밀지도_kyounglin.py
@@ -0,0 +1,23 @@
+# 비밀지도
+
+# 2021-01-17
+
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/17681
+
+def solution(n, arr1, arr2):
+    check=[]
+    answer=[]
+
+    for i in range(n):
+        check.append(bin(arr1[i]|arr2[i])[2:])
+
+    for j in range(len(check)):
+
+        answer.append(check[j].replace("1","#").replace("0"," "))
+
+    return answer
+
+print(solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]))
+print(solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]))
+
+
