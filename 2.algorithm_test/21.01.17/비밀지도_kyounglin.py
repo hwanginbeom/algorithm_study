be843310e1a8e42c91e339a448954872064f30b8
Kyounglin-Kim <kim232311@gmail.com>
2021-01-20 20:04:52 +0900

    2021.01.17 | add algorithm_test

--- 2.algorithm_test/21.01.17/비밀지도_kyounglin.py
+++ 2.algorithm_test/21.01.17/비밀지도_kyounglin.py
@@ -9,15 +9,13 @@ def solution(n, arr1, arr2):
     answer=[]
 
     for i in range(n):
-        check.append(bin(arr1[i]|arr2[i])[2:])
+        check.append(bin(arr1[i]|arr2[i])[2:].zfill(n))
 
     for j in range(len(check)):
-
         answer.append(check[j].replace("1","#").replace("0"," "))
 
     return answer
 
 print(solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]))
 print(solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]))
-
 
