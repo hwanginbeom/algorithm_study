eab61face15dedc46898aa862fef8debdf3ff169
Park SeJin <sjsj9368@gmail.com>
2021-02-03 01:49:59 +0900

    2021.02.03 | modify algorithm

--- 1.algorithm_question/1.greedy/64.Greedy_sejin.py
+++ 1.algorithm_question/1.greedy/64.Greedy_sejin.py
@@ -1,29 +1,36 @@
+# plus 길이 2 이상 minus 길이 2 이상
+# plus 길이 2 이상 : 1이 있는 경우, 나머지
+    # 1 2 => 더하는게 낫네
+# minus 길이 2 이상 : 0과 -1이 있는 경우, 나머지
+    # -1 0 =>  곱하는게 낫네
+    # -2 -1 => 곱하는게 낫네
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 plus = []
 minus = []
+answer = 0
+
 for _ in range(n) :
     m = int(input())
     if m > 0 :
         plus.append(m)
+    elif m == 1 :
+        answer += 1
     else :
         minus.append(m)
 plus.sort(reverse=True)
 minus.sort()
 
-def plus_2(x) :
-    return x[0]*x[1]
-def minus_2(x) :
-    return abs(x[0]*x[1])
+for i in range(0, len(plus)-1, 2) :
+    answer += plus[i] * plus[i+1]
+if len(plus) % 2 != 0 :
+    answer += plus[-1]
 
-if (len(plus)>1) & (len(minus)>1) :
-    if plus_2(plus) >= minus_2(minus) :
-        print(sum(minus)+plus_2(plus)+sum(plus[2:]))
-    else :
-        print(sum(plus) + minus_2(minus) + sum(minus[2:]))
-elif len(plus)>1 :
-    print('true')
-    print(sum(minus)+plus_2(plus)+sum(plus[2:]))
-elif len(minus)>1 :
-    print(sum(plus) + minus_2(minus) + sum(minus[2:]))
-else :
-    print(sum(plus)+sum(minus))
+for i in range(0, len(minus)-1, 2) :
+    answer += minus[i] * minus[i + 1]
+if len(minus) % 2 != 0 :
+    answer += minus[-1]
+print(answer)
