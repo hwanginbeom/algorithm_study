063cd779beb519d605e1f1431ca7117eae95914d
WooSeok Choi <wsc9150@gmail.com>
2021-02-15 00:15:25 +0900

    21.02.15 | add coding test

--- 2.algorithm_test/21.02.09/21.02.09_wooseok.py
+++ 2.algorithm_test/21.02.09/21.02.09_wooseok.py
@@ -21,7 +21,7 @@ def path_calc(n, current):
         return 4
 
 
-def solution(numbers, hand):
+def solution1(numbers, hand):
     answer = ''
 
     current_L = '*'
@@ -57,36 +57,92 @@ def solution(numbers, hand):
 
 numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
 hand = "left"
-print(solution(numbers, hand))
+# print(solution1(numbers, hand))
 
 
 
 # 순위 검색
-import re
+from bisect import bisect_left
+
+boolean_list = []
+tmp = []
+
+
+# 모든 경우의 수 True False 리스트 만들기
+def sixteen():
+    global boolean_list, tmp
+
+    if len(tmp) == 4:
+        t = []
+        for index in tmp:
+            t.append(index)
+        boolean_list.append(t)
+
+        return
+
+    for i in (False, True):
+        tmp.append(i)
+        sixteen()
+        tmp.pop()
 
 
 def solution(info, query):
+    global boolean_list
+
     answer = []
+    dict = {}
 
-    for q in query:
-        score = re.findall('\d+', q)
-        q_words = q.split('and')
-        print(q_words)
+    sixteen()
 
-        for i in info:
-            i_words = i.split(' ')
-            print(i_words)
+    # 지원자 분류
+    for i in info :
+        split_list = i.split(' ')
+        score = int(split_list.pop())
 
-            for j in q_words:
-                j = re.findall('\D+', j)
-                j = j[0].strip()
+        for j in range(len(boolean_list)) :
+            temp = split_list.copy()
 
-                for k in i_words:
-                    k = k.strip()
+            for k in range(4) :
+                if not boolean_list[j][k] :
+                    temp[k] = '-'
+
+            string = ''.join(temp)
+
+            if string not in dict.keys() :
+                dict[string] = [score]
+            else :
+                dict[string].append(score)
+
+    # 계속 효율성 반만 맞았던 이유
+    # 내장함수 sort도 시간복잡도를 차지하기 때문...
+    # 검색 조건을 사용하는 반복문에서 sort를 쓰게 되면
+    # 프로그램의 전체 시간복잡도가 늘어나는 것과 동일하므로 효율성은 낮아진다.
+    # 반복문 바깥에서 전체를 정렬하는 것이 더 효율이 높다.
+    for i in dict.keys() :
+        dict[i].sort()
+
+    # 검색 조건 사용
+    for i in query :
+        split_list = i.split(' ')
+        score = int(split_list.pop())
+        search = ''
+
+        for j in split_list :
+            if j != 'and' :
+                search += j
+
+        if search in dict.keys() :
+            length = len(dict[search])
+
+            # lower bound (bisect_left)
+            index = bisect_left(dict[search], score)
+            answer.append(length - index)
+        else :
+            answer.append(0)
 
     return answer
 
 
-info = ["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
-query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
+info = ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"]
+query = ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]
 print(solution(info, query))
