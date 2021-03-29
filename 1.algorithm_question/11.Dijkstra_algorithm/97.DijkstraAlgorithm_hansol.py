706dd256d921a008d5d28a7c32b8b93fb3aadcfb
HANSOL JEONG <losnah24@gmail.com>
2021-03-30 00:54:50 +0900

    21.03.29 | add Dijkstra Algorithm

---
+++ 1.algorithm_question/11.Dijkstra_algorithm/97.DijkstraAlgorithm_hansol.py
@@ -0,0 +1,52 @@
+# Dajikstra Algorithm
+# 파티
+# https://www.acmicpc.net/problem/1238
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m, x = map(int, input().split())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q: # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(x)
+x_distance = distance.copy()
+
+time_list = []
+
+for i in range(1, n + 1) :
+    if i == x :
+        continue
+    distance = [INF] * (n + 1)
+    s = 0
+    dijkstra(i)
+    s += distance[x] + x_distance[i]
+    time_list.append(s)
+
+print(max(time_list))
