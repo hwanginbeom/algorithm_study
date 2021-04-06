43073498cf3b787b480f5f50890f107c6e907e3e
WooSeok Choi <wsc9150@gmail.com>
2021-04-06 23:54:38 +0900

    21.04.06 | add PrimeNumber

---
+++ 1.algorithm_question/15.Prime_Number/101.PrimeNumber_wooseok.py
@@ -0,0 +1,46 @@
+# 골드바흐 파티션
+# https://www.acmicpc.net/problem/17103
+import math
+
+
+def solution() :
+    t = int(input())
+
+    answer = []
+    for _ in range(t) :
+        n = int(input())
+        _list = [True for i in range(n + 1)]
+
+        for i in range(2, int(math.sqrt(n)) + 1) :
+            if _list[i] :
+                j = 2
+
+                while i * j <= n :
+                    _list[i * j] = False
+                    j += 1
+
+        count = 0
+        center = n // 2
+
+        if _list[center] :
+            count += 1
+
+        left = center - 1
+        right = center + 1
+        while True :
+            if _list[left] and _list[right] :
+                count += 1
+
+            left -= 1
+            right += 1
+
+            if left == 1 :
+                break
+
+        answer.append(count)
+
+    for i in answer :
+        print(i)
+
+
+solution()
