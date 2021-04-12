8659be02a2f8e98e301536b619c1591502da2ad6
GyeongHyeon <lkh6035@naver.com>
2021-04-12 21:22:53 +0900

    21.04.12 | 후보키_gy

---
+++ 2.algorithm_test/21.04.12/후보키_gyeonghyeon.py
@@ -0,0 +1,33 @@
+from itertools import combinations as combi
+
+
+def solution(relation):
+    row = len(relation)
+    col = len(relation[0])
+
+    # 전체 조합
+    candidates = []
+    for i in range(1, col + 1):
+        print(list(combi(range(col), i)))
+        candidates.extend(combi(range(col), i))
+
+    # 유일성
+    unique = []
+    for candi in candidates:
+        tmp = [tuple([item[i] for i in candi]) for item in relation]
+        print(tmp)
+        if len(set(tmp)) == row:
+            unique.append(candi)
+
+    # 최소성
+    answer = set(unique)
+    for i in range(len(unique)):
+        for j in range(i + 1, len(unique)):
+            if len(unique[i]) == len(set(unique[i]) & set(unique[j])):
+                answer.discard(unique[j])
+    return len(answer)
+
+
+relation = [["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"],
+            ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]
+solution(relation)
