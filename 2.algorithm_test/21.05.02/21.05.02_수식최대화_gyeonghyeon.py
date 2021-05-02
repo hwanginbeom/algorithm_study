1a5c4d07dd127ce430269edf36cacf5cea70f4fb
GyeongHyeon <lkh6035@naver.com>
2021-05-02 19:44:54 +0900

    21.05.02 | add_수식최대화

---
+++ 2.algorithm_test/21.05.02/21.05.02_수식최대화_gyeonghyeon.py
@@ -0,0 +1,15 @@
+def solution(expression):
+    operand = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operand:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
+
+expression = "100-200*300-500+20"
+solution(expression)
