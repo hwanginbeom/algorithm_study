f848cc0b1132365453df9bbf506b948edcf2a5b9
GyeongHyeon <lkh6035@naver.com>
2021-07-11 18:32:42 +0900

    21.07.11 | add_test

---
+++ 2.algorithm_test/21.07.11/21.07.11_숫자문자열과_영단어_gyeonghyeon.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+eng_list = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+            'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
+
+def solution(s):
+    for idx, value in eng_list.items():
+        s = s.replace(idx, str(value))
+    return int(s)
+
+
+s = "one4seveneight"
+print(solution(s))
