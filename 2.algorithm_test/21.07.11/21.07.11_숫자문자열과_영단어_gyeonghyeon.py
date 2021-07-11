554751d80e7ce51769be4af2f6b837452913737c
GyeongHyeon <lkh6035@naver.com>
2021-07-11 18:34:58 +0900

    21.07.11 | add_test

--- 2.algorithm_test/21.07.11/21.07.11_숫자문자열과_영단어_gyeonghyeon.py
+++ 2.algorithm_test/21.07.11/21.07.11_숫자문자열과_영단어_gyeonghyeon.py
@@ -5,7 +5,20 @@ input = sys.stdin.readline
 eng_list = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
             'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
+def solution(s):
+    answer = word = ''
+    for text in s:
+        if text.isdigit():
+            answer += text
+        else:
+            word += text
+            if word in eng_list.keys():
+                answer += str(eng_list[word])
+                word = ''
+    return int(answer)
 
+### replace로 구상했다가 안됐던 풀이
+### 이렇게 하면 된다는걸 알아서 같이 작성
 def solution(s):
     for idx, value in eng_list.items():
         s = s.replace(idx, str(value))
