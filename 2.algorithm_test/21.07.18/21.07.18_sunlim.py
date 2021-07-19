b4cc46220eb0e9c2f62046200cd54667ab809242
kimsunlim <rlatjsfla15@naver.com>
2021-07-19 22:58:58 +0900

    21.07.19|add coding test

---
+++ 2.algorithm_test/21.07.18/21.07.18_sunlim.py
@@ -0,0 +1,20 @@
+
+def solution(str1, str2):
+
+    list1 = [str1[n:n+2].lower() for n in range(len(str1)-1) if str1[n:n+2].isalpha()]
+    list2 = [str2[n:n+2].lower() for n in range(len(str2)-1) if str2[n:n+2].isalpha()]
+
+    tlist = set(list1) | set(list2)
+    res1 = [] #합집합
+    res2 = [] #교집합
+
+    if tlist:
+        for i in tlist:
+            res1.extend([i]*max(list1.count(i), list2.count(i)))
+            res2.extend([i]*min(list1.count(i), list2.count(i)))
+
+        answer = int(len(res2)/len(res1)*65536)
+        return answer
+
+    else:
+        return 65536
