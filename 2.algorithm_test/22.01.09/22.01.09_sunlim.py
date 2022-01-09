35fe0100d5c0698134531c6eb1a00c351a443f2d
kimsunlim <rlatjsfla15@naver.com>
2022-01-09 23:39:15 +0900

    22.01.09 |add test

---
+++ 2.algorithm_test/22.01.09/22.01.09_sunlim.py
@@ -0,0 +1,25 @@
+def solution(new_id):
+    # 1단계
+    new_id = new_id.lower()
+    # 2단계
+    answer = ''
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4단계
+    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
+    answer = answer[:-1] if answer[-1] == '.' else answer
+    # 5단계
+    answer = 'a' if answer == '' else answer
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7단계
+    if len(answer) <= 3:
+        answer = answer + answer[-1] * (3-len(answer))
+    return answer
